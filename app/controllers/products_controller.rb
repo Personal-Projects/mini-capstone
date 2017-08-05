63f0a0d03b22dddb8e432ca97f5299896ccfebec
jasmine <jasliu2012@gmail.com>
2017-08-05 15:41:12 -0500

    updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,16 @@ class ProductsController < ApplicationController
     @products = Product.all
     render "product.html.erb"
   end
+
+  def show
+    # show the user the form
+    render 'show.html.erb'
+  end
+
+  def make
+    #that info is sent through params to another action/url
+    product4 = Product.new(name: params[:name], price: params[:price], description: params[:description])
+    render 'make_the_product.html.erb'
+    product4.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/product' => 'products#product'
+  get '/new_product' => 'products#show'
+  post '/make_the_product' => 'products#make'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
