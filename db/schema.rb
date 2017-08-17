1c47b30e920ecd50232d0d0caaf24344546688b0
jasmine <jasliu2012@gmail.com>
2017-08-17 16:44:09 -0500

    Successfully added images to index and show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817001053) do
+ActiveRecord::Schema.define(version: 20170817210932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "images", force: :cascade do |t|
-    t.string "image"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do
-  Product.create(
-    name: Faker::Pokemon.name,
-    location: Faker::Pokemon.location,
-    price: Faker::Commerce.price,
-    description: Faker::Pokemon.move
-  )
+# 100.times do
+#   Product.create(
+#     name: Faker::Pokemon.name,
+#     location: Faker::Pokemon.location,
+#     price: Faker::Commerce.price,
+#     description: Faker::Pokemon.move
+#   )
+# end
+
+# products = Product.all
+# products.each do |product|
+#   product.supplier_id = 1
+#   product.save
+# end
+
+# products = Product.all
+# suppliers = Supplier.all
+# products.each do |product|
+#   product.supplier_id = suppliers.sample.id
+#   product.save
+# end
+
+# # find the supplier of a product
+# product = Product.first # => instance of product
+# the_supplier_id = product.supplier_id # => an integer
+# Supplier.find_by(id: the_supplier_id) # => instance of supplier
+
+products = Product.all
+products.each do |product|
+  image = Image.new(image: Faker::Avatar.image, product_id: product.id)
+  image.save
 end
+
+
+
+
+
+
+
